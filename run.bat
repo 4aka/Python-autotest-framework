5d8c57add2aa03aaf64eadf4bab3102483b3c652
4aka <7nuclear@gmail.com>
2024-05-20 13:44:55 +0300

    Update requirements and run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 pytest ^
-tests/create_post.py ^
-tests/delete_post.py ^
-tests/edit_post.py ^
-tests/get_post.py ^
+tests/api/create_post.py ^
+tests/api/delete_post.py ^
+tests/api/edit_post.py ^
+tests/api/get_post.py ^
 --html=report.html --self-contained-html
 
 start file:///E:/Projects/python/autoTestFramework/report.html
