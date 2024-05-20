8391a349e5654a999cb457d743c9fe350689b0f4
4aka <7nuclear@gmail.com>
2024-05-20 16:52:28 +0300

    Update test

--- tests/ui/google_search_tests.py
+++ tests/ui/google_search_tests.py
@@ -4,9 +4,15 @@ from pages.google_search_page import SearchHelper
 # https://selenium-python.readthedocs.io/
 
 def test_google_search(browser):
-    google_main_page = SearchHelper(browser)
-    google_main_page.go_to_site()
-    google_main_page.enter_word("Hello")
-    google_main_page.click_on_the_search_button()
 
-    # assert "Картинки" and "Видео" in elements
+    # TODO exclude steps below from the test
+    google_page = SearchHelper(browser)
+    google_page.go_to_site()
+
+    google_page.enter_word("Automated tests")
+    google_page.click_on_the_search_button()
+    google_page.waiting_for_results_list()
+
+    elements = google_page.get_results_list()
+
+    assert len(elements) > 1
