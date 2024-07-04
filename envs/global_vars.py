9cff459881a2388afd124d28fa94e191a91d6d6d
4aka <7nuclear@gmail.com>
2024-07-04 13:48:39 +0300

    Add global variables

---
+++ envs/global_vars.py
@@ -0,0 +1,6 @@
+from envs import load_env
+
+BASE_URL = load_env.get_from_env('BASE_URL')
+UI_BASE_URL = load_env.get_from_env('UI_BASE_URL')
+BROWSER_NAME = load_env.get_from_env('BROWSER_NAME')
+LOG_LEVEL = load_env.get_from_env('LOG_LEVEL')
