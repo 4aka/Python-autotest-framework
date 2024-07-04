a07a8ab85f9b98557f073c4e1cd2f011e68930cd
4aka <7nuclear@gmail.com>
2024-07-04 13:47:27 +0300

    Delete old files

--- run.bat
+++
@@ -1,8 +0,0 @@
-pytest ^
-tests/api/create_post.py ^
-tests/api/delete_post.py ^
-tests/api/edit_post.py ^
-tests/api/get_post.py ^
---html=report.html --self-contained-html
-
-start file:///E:/Projects/python/autoTestFramework/report.html
