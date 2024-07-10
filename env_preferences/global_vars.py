9f6ea5b9a40897d981f270da67123c8d157af4ff
4aka <7nuclear@gmail.com>
2024-07-10 23:40:55 +0300

    Update global variables

--- env_preferences/global_vars.py
+++ env_preferences/global_vars.py
@@ -1,6 +1,9 @@
 from env_preferences import load_env
 
 BASE_URL = load_env.get_from_env('BASE_URL')
+NP_BASE_URL = load_env.get_from_env('NP_BASE_URL')
 UI_BASE_URL = load_env.get_from_env('UI_BASE_URL')
 BROWSER_NAME = load_env.get_from_env('BROWSER_NAME')
 LOG_LEVEL = load_env.get_from_env('LOG_LEVEL')
+API_KEY = load_env.get_from_env('API_KEY')
+PHONE_NUMBER = load_env.get_from_env('PHONE_NUMBER')
