534ecbaa5bc651b9df26688a48006b0f724330ac
4aka <7nuclear@gmail.com>
2024-07-11 12:10:20 +0300

    Add get random int

--- fake_functions/faker.py
+++ fake_functions/faker.py
@@ -3,6 +3,8 @@ import random
 import string
 from faker import Faker
 
+fake = Faker()
+
 
 class Fake:
 
@@ -12,10 +14,13 @@ class Fake:
 
     @staticmethod
     def fake_id():
-        fake = Faker()
         return int(fake.random_int(min=1, max=100))
 
     @staticmethod
     def fake_fixed_string(string_size):
         characters = string.ascii_letters + string.digits + string.punctuation
         return ''.join(random.choice(characters) for _ in range(string_size))
+
+    @staticmethod
+    def fake_int():
+        return int(fake.random_int(min=1, max=99999999999999999))
