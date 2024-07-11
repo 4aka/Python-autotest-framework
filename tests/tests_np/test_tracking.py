a1fdcb21a0580c032cf881904f746ed2f173a07b
4aka <7nuclear@gmail.com>
2024-07-11 11:03:45 +0300

    Update test with parsing

--- tests/tests_np/test_tracking.py
+++ tests/tests_np/test_tracking.py
@@ -1,6 +1,7 @@
 import pytest
 from framework.api.model_builder.np_tracking_builder import TrackingBuilder as builder
 from framework.api.service.np_common_servise import Request as action
+from framework.api.models.np_tracking_response import TrackingResponse
 
 
 @pytest.fixture(scope='function')
@@ -13,7 +14,7 @@ def setup_function():
 @pytest.mark.api
 def test_create_post(setup_module, setup_function):
 
-    tracking_number = '20450959314437'
+    tracking_number = ''
 
     # Build model
     tracking = builder.build_tracking_request(tracking_number)
@@ -22,16 +23,13 @@ def test_create_post(setup_module, setup_function):
     response = action.send_post_request(tracking)
     assert response.status_code == 200
 
-    print(response.json())
-
     # Parse response
-    # response_model = Post.model_validate(response.json())
-    #
-    # # Assert equals each field
-    # assert response_model.userId == post_data.userId
-    # assert response_model.id == post_data.id
-    # assert response_model.title == post_data.title
-    # assert response_model.body == post_data.body
+    tracking_response = TrackingResponse.model_validate(response.json())
+    parcel = tracking_response.data[0]
+
+    # Assertions
+    assert tracking_response.success
+    assert parcel.Number == tracking_number
 
 
 if __name__ == '__main__':
