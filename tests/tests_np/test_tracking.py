23648c0b070e1fa9f8c499dfc68466f543b0eec7
4aka <7nuclear@gmail.com>
2024-07-11 12:10:55 +0300

    Add test where parcel number is incorrect

--- tests/tests_np/test_tracking.py
+++ tests/tests_np/test_tracking.py
@@ -2,6 +2,7 @@ import pytest
 from framework.api.model_builder.np_tracking_builder import TrackingBuilder as builder
 from framework.api.service.np_common_servise import Request as action
 from framework.api.models.np_tracking_response import TrackingResponse
+from fake_functions.faker import Fake
 
 
 @pytest.fixture(scope='function')
@@ -12,7 +13,7 @@ def setup_function():
 
 
 @pytest.mark.api
-def test_create_post(setup_module, setup_function):
+def test_parcel_tracking(setup_module, setup_function):
 
     tracking_number = '20450959314437'
 
@@ -31,6 +32,25 @@ def test_create_post(setup_module, setup_function):
     assert tracking_response.success
     assert parcel.Number == tracking_number
 
+
+@pytest.mark.api
+def test_parcel_number_is_incorrect(setup_module, setup_function):
+
+    tracking_number = Fake.fake_int()
+
+    # Build model
+    tracking = builder.build_tracking_request(tracking_number)
+
+    # Send request and assert 200
+    response = action.send_post_request(tracking)
+    assert response.status_code == 200
+
+    # Parse response
+    tracking_response = TrackingResponse.model_validate(response.json())
+
+    # Assertions
+    assert not tracking_response.success
+    assert tracking_response.errors[0] == 'Document number is not correct'
 
 if __name__ == '__main__':
     pytest.main()
