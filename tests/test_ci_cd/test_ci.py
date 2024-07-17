871185c3787c11dc471ee6c9fbf494acf786f596
4aka <7nuclear@gmail.com>
2024-07-17 15:15:36 +0300

    Update tests

--- tests/test_ci_cd/test_ci.py
+++ tests/test_ci_cd/test_ci.py
@@ -3,9 +3,11 @@ import os
 
 def test_print_browser_name_from_ci():
     browser_name = os.getenv('BROWSER_NAME')
+    print(browser_name)
     assert browser_name is not None, "BROWSER_NAME is not set"
 
 
 def test_print_log_level_from_ci():
     log_level = os.getenv('LOG_LEVEL')
-    assert log_level is not None, "LOG_LEVEL is not set"
+    print(log_level)
+    assert log_level is None, "LOG_LEVEL is set"
